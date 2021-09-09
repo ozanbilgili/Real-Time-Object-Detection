fe111a528b63c0eec50d5426d671d48fab51b1fe
ozanbilgili <ozan.bilgili@hotmail.com>
2021-09-09 17:56:58 +0300

    Initial Commit

---
+++ Real Time Object Detection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Real Time Object Detection
+//
+//  Created by Ozan Bilgili on 9.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Real Time Object DetectionTests/Real_Time_Object_DetectionTests.swift
@@ -0,0 +1,33 @@
+//
+//  Real_Time_Object_DetectionTests.swift
+//  Real Time Object DetectionTests
+//
+//  Created by Ozan Bilgili on 9.09.2021.
+//
+
+import XCTest
+@testable import Real_Time_Object_Detection
+
+class Real_Time_Object_DetectionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
